1a2b6954ec8cfbdcf0f6bce1a5a71d0b14995b1b
sadanandprabhu7 <sadanandprabhu7@gmail.com>
2022-10-25 00:24:08 +0530

    dom file added

---
+++ dom.js
@@ -0,0 +1,27 @@
+//console.dir(document);
+// console.log(document.domain);
+// console.log(document.URL);
+// console.log(document.title);
+// console.log(document.doctype);
+// console.log(document.head);
+// console.log(document.body);
+//console.log(document.all);
+//console.log(document.all[10]);
+//document.all[10].textContent='hello';
+//console.log(document.forms[0]);
+//console.log(document.links[0]);
+//console.log(document.images);
+//console.log(document.getElementById('header-title'));
+// var header = document.getElementById('header-title');
+// console.log(header.innerText);
+// console.log(header.textContent);
+// console.log(header.innerHTML);
+// header.textContent='hi';
+// header.innerHTML='bye';
+//console.log(document.title);
+let headerBorder = document.getElementById('header-title');
+headerBorder.style.border='solid black 3px';
+
+let bold = document.getElementsByClassName('title');
+bold[0].style.fontWeight='bold';
+bold[0].style.color='green';
